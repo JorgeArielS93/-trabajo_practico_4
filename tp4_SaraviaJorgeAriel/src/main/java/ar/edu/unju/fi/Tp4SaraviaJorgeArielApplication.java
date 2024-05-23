31503130e39d165cad6122c361bff55f26ce0ea0
ArielS <diciembre93@gmail.com>
2024-05-23 14:40:06 -0300

    archivos base y librerias para resolver el tp

---
+++ tp4_SaraviaJorgeAriel/src/main/java/ar/edu/unju/fi/Tp4SaraviaJorgeArielApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp4SaraviaJorgeArielApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp4SaraviaJorgeArielApplication.class, args);
+	}
+
+}
