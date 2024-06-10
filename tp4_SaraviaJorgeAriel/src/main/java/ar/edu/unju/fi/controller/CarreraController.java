76a9ca42e4867187f2e0b8c0a89d76e5f6c10018
ArielS <diciembre93@gmail.com>
2024-06-10 19:57:40 -0300

    se completo el abm de Carrera

--- tp4_SaraviaJorgeAriel/src/main/java/ar/edu/unju/fi/controller/CarreraController.java
+++ tp4_SaraviaJorgeAriel/src/main/java/ar/edu/unju/fi/controller/CarreraController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -14,28 +15,48 @@ import ar.edu.unju.fi.model.Carrera;
 @Controller
 @RequestMapping("/carrera")
 public class CarreraController {
-	@Autowired
-	private Carrera unaCarrera;
-	
-	@GetMapping("/formuarioCarrera")
-	public ModelAndView getFormCarrera() {
-		// vista formCarrera.html
-		ModelAndView modelView = new ModelAndView("formCarrera");
-		// agregar el objeto
-		modelView.addObject("carrera",unaCarrera);
-		return modelView;
-	}
-	
-	@GetMapping ("/listaCarrera")
-	public ModelAndView listaCarreras() {
-		ModelAndView modelView = new ModelAndView("listaCarreras");
-		modelView.addObject("listaCarreras",ListadoCarreras.getCarreras());
-		return modelView;
-	}
-	
-	@PostMapping("/guardar")
-	public ModelAndView postPageSaveCarrera(@ModelAttribute("carrera") Carrera c) {
-		return null;
-		
-	}
+
+    @Autowired
+    private Carrera unaCarrera;
+    
+    @GetMapping("/formularioCarrera")
+    public ModelAndView getFormCarrera() {
+        ModelAndView modelView = new ModelAndView("formCarrera");
+        modelView.addObject("carrera", unaCarrera);
+        modelView.addObject("isEdit", false);
+        return modelView;
+    }
+
+    @GetMapping("/listaCarreras")
+    public ModelAndView listaCarreras() {
+        ModelAndView modelView = new ModelAndView("listaCarreras");
+        modelView.addObject("listaCarreras", ListadoCarreras.getCarreras());
+        return modelView;
+    }
+
+    @PostMapping("/guardar")
+    public ModelAndView postPageSaveCarrera(@ModelAttribute("carrera") Carrera c) {
+        if (c.getCodigo() != null && !c.getCodigo().isEmpty()) {
+            ListadoCarreras.actualizarCarrera(c);
+        } else {
+            ListadoCarreras.agregarCarrera(c);
+        }
+        ModelAndView mv = new ModelAndView("redirect:/carrera/listaCarreras");
+        return mv;
+    }
+
+    @GetMapping("/modificar/{codigo}")
+    public ModelAndView modificarCarrera(@PathVariable("codigo") String codigo) {
+        ModelAndView mv = new ModelAndView("formCarrera");
+        mv.addObject("carrera", ListadoCarreras.findCarreraByCodigo(codigo));
+        mv.addObject("isEdit", true);
+        return mv;
+    }
+
+    @GetMapping("/borrar/{codigo}")
+    public ModelAndView borrarCarrera(@PathVariable("codigo") String codigo) {
+        ListadoCarreras.eliminarCarrera(codigo);
+        ModelAndView mv = new ModelAndView("redirect:/carrera/listaCarreras");
+        return mv;
+    }
 }
